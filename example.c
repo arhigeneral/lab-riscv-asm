fff7e1c6c6207ca2eb65e295a9a70120db530197
Your Name <sad_casd@mail.ru>
2022-06-09 14:59:35 +0300

    init

---
+++ example.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define N 4
+#define M 2
+
+void process(int n, int m, int matrix [M][N], int *result)
+{
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (matrix[i][j] > 0){
+                *result += matrix[i][j];
+            }
+            
+        }
+    }
+
+}
+
+int main(int argc, char** argv)
+{
+    int matrix[M][N];
+    int result = 0;
+    
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    process(N, M, matrix, &result);
+
+
+    printf("%d ", result);
+
+
+    return 0;
+}
